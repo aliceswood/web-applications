0bf9638fd4f78f0bc2cd73e3bb19e5ff35a8ac2f
Alice Wood <alicesophiewood@gmail.com>
2023-05-02 13:43:52 +0100

    challenge 2 - building a route

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -5,4 +5,34 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  # GET /
+  #  Root path (homepage, index page)
+  get '/' do
+    return 'Hello!'
+  end
+
+  get '/posts' do
+    name = params[:name]
+    cohort_name = params[:cohort_name]
+
+    return "Hello #{name}, you are in the #{cohort_name} cohort"
+  end
+
+  post '/posts' do
+    title = params[:title]
+    return "Post was created with the title: #{title}"
+  end
+
+  get '/hello' do
+    name = params[:name]
+    return "Hello #{name}"
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you send this message: '#{message}'"
+  end
 end
